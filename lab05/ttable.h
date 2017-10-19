cf718c2a56a9a9514a3dc9bbaa5850030133951a
Simon <simon.m.belete@gmail.com>
2017-10-19 16:13:19 -0700

    removed labs

--- lab05/ttable.h
+++
@@ -1,9 +0,0 @@
-#ifndef __TTABLE_H
-#define __TTABLE_H
-
-void ttable_init();
-int ttable_insert(char *);
-void ttable_list();
-void ttable_destroy();
-
-#endif
--- lab11/fractals/fractal.h
+++
@@ -1,6 +0,0 @@
-#ifndef FRACTAL_H_
-#define FRACTAL_H_
-
-int generate_fractal(char *file, float width, float height, int workers);
-
-#endif  // FRACTAL_H_
